707da55f197bfa684ea7a5c99d902fb9f147fe40
zxd882002 <zxd882002@126.com>
2025-10-15 14:21:17 +0800

    Complete the header design

--- ProductMonitor/ViewModels/MainWindowViewModel.cs
+++ ProductMonitor/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace ProductMonitor.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "生成监控平台";
+        private string _title = "生产监控平台";
 
         public string Title
         {
@@ -12,7 +12,7 @@ namespace ProductMonitor.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        private string _subTitle = "一个天花板极的MES系统";
+        private string _subTitle = "相信自己，你可以的";
 
         public string SubTitle
         {
