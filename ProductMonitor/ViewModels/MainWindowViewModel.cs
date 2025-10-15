a0b01027ad286fb4d279df359891990bc3390d82
zxd882002 <zxd882002@126.com>
2025-10-15 11:20:33 +0800

    Complete the header icon and title

--- ProductMonitor/ViewModels/MainWindowViewModel.cs
+++ ProductMonitor/ViewModels/MainWindowViewModel.cs
@@ -4,16 +4,24 @@ namespace ProductMonitor.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "生成监控平台";
+
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
+        private string _subTitle = "一个天花板极的MES系统";
+
+        public string SubTitle
+        {
+            get => _subTitle;
+            set => SetProperty(ref _subTitle, value);
+        }
+
         public MainWindowViewModel()
         {
-
         }
     }
 }
