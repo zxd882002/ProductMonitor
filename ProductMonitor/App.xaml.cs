ce35ae43c17f3bc23958122dafcd9a32fc811abb
zxd882002 <zxd882002@126.com>
2025-10-15 10:16:09 +0800

    Create a Project and a high-level structure design

---
+++ ProductMonitor/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Services.Dialogs;
+using ProductMonitor.ViewModels;
+using ProductMonitor.Views;
+
+namespace ProductMonitor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterDialog<LoginUserControl, LoginUserControlViewModel>();
+        }
+
+        protected override void OnInitialized()
+        {
+            var dialog = Container.Resolve<IDialogService>();
+            dialog.ShowDialog("LoginUserControl", r =>
+            {
+                if (r.Result != ButtonResult.OK)
+                {
+                    Environment.Exit(0);
+                    return;
+                }
+            });
+
+            base.OnInitialized();
+        }
+    }
+}
---
+++ ProductMonitor/ViewModels/LoginUserControlViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+
+namespace ProductMonitor.ViewModels
+{
+    internal class LoginUserControlViewModel : IDialogAware
+    {
+        public string Title => "Login";
+
+        public event Action<IDialogResult> RequestClose;
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+
+        public DelegateCommand LoginCommand { get; set; }
+
+        public LoginUserControlViewModel()
+        {
+            LoginCommand = new DelegateCommand(DoLogin);
+        }
+
+        private void DoLogin()
+        {
+            RequestClose?.Invoke(new DialogResult(ButtonResult.OK));
+        }
+    }
+}
