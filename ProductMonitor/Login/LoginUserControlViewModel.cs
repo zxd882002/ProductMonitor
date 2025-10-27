4bb8c9908b93271fa08cebade5687f499f9fe86a
zxd882002 <zxd882002@126.com>
2025-10-27 16:28:00 +0800

    modify login page

--- ProductMonitor/Login/LoginUserControlViewModel.cs
+++ ProductMonitor/Login/LoginUserControlViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
-
+using System.Windows;
 using Prism.Commands;
 using Prism.Dialogs;
+using Prism.Mvvm;
 
 namespace ProductMonitor.Login
 {
-    internal class LoginUserControlViewModel : IDialogAware
+    internal class LoginUserControlViewModel : BindableBase, IDialogAware
     {
         private DialogCloseListener _requestClose;
         DialogCloseListener IDialogAware.RequestClose => _requestClose;
-        
+
         public string Title => "Login";
+
+        private int _selectedIndex;
+
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set => SetProperty(ref _selectedIndex, value);
+        }
 
         public bool CanCloseDialog()
         {
@@ -25,12 +34,40 @@ namespace ProductMonitor.Login
         {
         }
 
-
         public DelegateCommand LoginCommand { get; set; }
+        public DelegateCommand RegisterCommand { get; set; }
+        public DelegateCommand NavigateRegisterCommand { get; set; }
+        public DelegateCommand NavigateForgetPasswordCommand { get; set; }
+        public DelegateCommand NavigateLoginCommand { get; set; }
 
         public LoginUserControlViewModel()
         {
+            _selectedIndex = 0;
             LoginCommand = new DelegateCommand(DoLogin);
+            RegisterCommand = new DelegateCommand(DoRegister);
+            NavigateRegisterCommand = new DelegateCommand(NavigateRegister);
+            NavigateForgetPasswordCommand = new DelegateCommand(NavigateForgetPassword);
+            NavigateLoginCommand = new DelegateCommand(NavigateLogin);
+        }
+
+        private void NavigateLogin()
+        {
+            SelectedIndex = 0;
+        }
+
+        private void NavigateRegister()
+        {
+            SelectedIndex = 1;
+        }
+
+        private void NavigateForgetPassword()
+        {
+            MessageBox.Show("Sorry，该功能不支持！");
+        }
+
+        private void DoRegister()
+        {
+            MessageBox.Show("注册成功");
         }
 
         private void DoLogin()
